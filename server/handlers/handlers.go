62461de88e6ffee9237e32ddcc69bdbcc1c366b3
Scott Richardson <scottjr632@gmail.com>
2021-07-07 18:39:16 -0500

    Adds api to create messages

--- server/handlers/handlers.go
+++ server/handlers/handlers.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"context"
 	"database/sql"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/scottjr632/chatback/server/broker"
 	"github.com/scottjr632/chatback/server/models"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 type Handlers struct {
@@ -26,5 +29,32 @@ func (h *Handlers) GetMessages(c *fiber.Ctx) error {
 		return err
 	}
 	c.JSON(messages)
+	return nil
+}
+
+func (h *Handlers) NewMessage(c *fiber.Ctx) error {
+	msg := models.Message{}
+	if err := c.BodyParser(&msg); err != nil {
+		c.JSON(&fiber.Map{
+			"status": false,
+			"error":  err,
+		})
+		return err
+	}
+
+	if err := msg.Insert(context.TODO(), h.db, boil.Infer()); err != nil {
+		log.Println(err)
+		c.JSON(&fiber.Map{
+			"status": false,
+			"error":  err,
+		})
+		return err
+	}
+
+	broadcast <- msg
+	c.JSON(&fiber.Map{
+		"status": true,
+		"error":  nil,
+	})
 	return nil
 }
